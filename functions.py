ede7fc0da42c19eb40e45d9fe0923ff6ec7073e4
Splanex <piopapapigrafo@gmail.com>
2022-11-13 20:26:50 +0000

    Updata: List comprehension

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ from os.path import exists
 import sys
 import glob
 import concurrent.futures
+import corrections
 
 #1
 def name_gen(file_name):
@@ -22,21 +23,14 @@ def get_hotpixels(hdul, limit):
     return np.argwhere(data >= limit)
 
 #3
-def remove_hotpixels(hdu, hot_pixels, raio=1): #Utiliza 1
+def remove_hotpixels(hdu, hot_pixels, width=1): #Utiliza 1
     a = np.copy(hdu["PRIMARY"].data)
     x_res,y_res = a.shape
     for k in hot_pixels:
         line = k[0]
         collumn = k[1]
-        vizinhos = []
-        for i in range(-raio, raio+1):
-            for j in range(-raio, raio+1):
-                    if ((line+i>=0 and collumn+j>=0 and line+i<x_res and collumn+j<y_res) and (not(i==0 and j==0))):
-                        vizinhos.append(a[line+i][collumn+j])
-
-        vizinhos = np.array(vizinhos)
-        mediana = np.median(vizinhos)
-
+        neighbours = np.array([a[line+i][collumn+j] for i in range(-width, width+1) for j in range(-width, width+1) if ((line+i>=0 and collumn+j>=0 and line+i<x_res and collumn+j<y_res) and (not(i==0 and j==0)))])
+        mediana = np.median(neighbours)
         a[line][collumn] = mediana
 
     file_name = hdu.filename()
@@ -50,27 +44,20 @@ def remove_hotpixels(hdu, hot_pixels, raio=1): #Utiliza 1
         print(e)
 
 #4
-def get_filtered_image_data(hdu_data, hot_pixels, raio):
+def get_filtered_image_data(hdu_data, hot_pixels, width):
     a = np.copy(hdu_data)
     x_res,y_res = a.shape
     for k in hot_pixels:
         line = k[0]
         collumn = k[1]
-        vizinhos = []
-        for i in range(-raio, raio+1):
-            for j in range(-raio, raio+1):
-                    if ((line+i>=0 and collumn+j>=0 and line+i<x_res and collumn+j<y_res) and (not(i==0 and j==0))):
-                        vizinhos.append(a[line+i][collumn+j])
-
-        vizinhos = np.array(vizinhos)
-        mediana = np.median(vizinhos)
-
+        neighbours = np.array([a[line+i][collumn+j] for i in range(-width, width+1) for j in range(-width, width+1) if ((line+i>=0 and collumn+j>=0 and line+i<x_res and collumn+j<y_res) and (not(i==0 and j==0)))])
+        mediana = np.median(neighbours)
         a[line][collumn] = mediana
 
     return a
 
 #5
-def get_ADA_HV(hdu, hot_pixels, raio):
+def get_ADA_HV(hdu, hot_pixels, width):
     a = np.copy(hdu["PRIMARY"].data)
     abs_diff_average = []
     hotpixels_values = []
@@ -78,29 +65,20 @@ def get_ADA_HV(hdu, hot_pixels, raio):
         line = k[0]
         collumn = k[1]
         hotpixels_values.append(a[line][collumn])
-        total = 0
-        n_vizinhos = 0
-        for i in range(-raio, raio+1):
-            for j in range(-raio, raio+1):
-                    if ((line+i>=0 and collumn+j>=0 and line+i<len(a) and collumn+j<len(a[0])) and (not(i==0 and j==0))):
-                        total += abs(a[line][collumn] - a[line+i][collumn+j])
-                        n_vizinhos += 1
-        abs_diff_average.append(total/n_vizinhos)
+
+        neighbours = np.array([abs(a[line][collumn] - a[line+i][collumn+j]) for i in range(-width, width+1) for j in range(-width, width+1) if ((line+i>=0 and collumn+j>=0 and line+i<len(a) and collumn+j<len(a[0])) and (not(i==0 and j==0)))])
+        total1 = np.sum(neighbours)
+        n_neighbours = len(neighbours)
+
+
+        abs_diff_average.append(total/n_neighbours)
 
     return abs_diff_average, hotpixels_values
 
-#6
-def scatter_plot(hotpixels_values,average_abs_diff, k):
-    plt.scatter(hotpixels_values,average_abs_diff)
-    plt.xlabel('Hot values')
-    plt.ylabel('Absolute difference average')
-    plt.title("K = {}".format(k))
-    plt.show()
-
 #7
-def get_stacked_image(darkframe, imgs_direct, std_mult=2, raio=1): #2,4
+def get_stacked_image(darkframe, imgs_direct, std_mult=2, width=1): #2,4
     print(f"Multiplicador de desvio = {std_mult}")
-    print(f"Raio = {raio}")
+    print(f"width = {width}")
     #calcular hot_pixels
     darkframe_data = np.copy(darkframe["PRIMARY"].data)
     limit = np.mean(darkframe_data) + std_mult*np.std(darkframe_data)
@@ -111,14 +89,20 @@ def get_stacked_image(darkframe, imgs_direct, std_mult=2, raio=1): #2,4
     for img in imgs:
         with fits.open(img) as image:
             print(f"A processar {img}...")
-            final_image += get_filtered_image_data(image["PRIMARY"].data, hot_pixels, raio) #Adicionar a final_image os valores de cada imagem filtrada
+            final_image += get_filtered_image_data(image["PRIMARY"].data, hot_pixels, width) #Adicionar a final_image os valores de cada imagem filtrada
     final_image = final_image//len(imgs) #Calcular a media de todas as imagens filtradas
     #Guardar os dados num ficherio fits
     hdu = fits.PrimaryHDU(final_image)
     hdu.writeto(imgs_direct+"stacked.fits", overwrite=True)
 
-#8
-def plot_ADA(hdu, darkframe, multiplicadores= [0, 0.5, 1, 1.5, 2, 2.5, 3],raio=1): #2,5
+def scatter_plot(hotpixels_values,average_abs_diff, k):
+    plt.scatter(hotpixels_values,average_abs_diff)
+    plt.xlabel('Hot values')
+    plt.ylabel('Absolute difference average')
+    plt.title("K = {}".format(k))
+    plt.show()
+
+def plot_ADA(hdu, darkframe, multiplicadores= [0, 0.5, 1, 1.5, 2, 2.5, 3],width=1): #2,5
     """average_abs_diff = []
     hotpixels_values = []"""
     for i in multiplicadores:
@@ -127,143 +111,28 @@ def plot_ADA(hdu, darkframe, multiplicadores= [0, 0.5, 1, 1.5, 2, 2.5, 3],raio=1
         limit = np.mean(darkframe_copy) + i*np.std(darkframe_copy)
         hot_pixels = get_hotpixels(darkframe, limit)
 
-        avg, hot_values = get_ADA_HV(hdu, hot_pixels, raio) #Obtem os AVG e os valores dos hot_pixels para cada multiplicador
+        avg, hot_values = get_ADA_HV(hdu, hot_pixels, width) #Obtem os AVG e os valores dos hot_pixels para cada multiplicador
         """
         average_abs_diff.append(avg)
         hotpixels_values.append(hot_values)"""
         scatter_plot(hot_values,avg, i)
 
 #9
-def get_true_hotpixels(hdul, hot_pixels, media, desvio, raio=1):
+def get_true_hotpixels(hdul, hot_pixels, media, desvio, width=1):
     a = np.copy(hdul["PRIMARY"].data)
     true_hotpixels = []
     for k in hot_pixels:
         line = k[0]
         collumn = k[1]
-        total = 0
-        n_vizinhos = 0
-        for i in range(-raio, raio+1):
-            for j in range(-raio, raio+1):
-                    if ((line+i>=0 and collumn+j>=0 and line+i<len(a) and collumn+j<len(a[0])) and (not(i==0 and j==0))):
-                        total += abs(a[line][collumn] - a[line+i][collumn+j])
-                        n_vizinhos += 1
-        ADA = total/n_vizinhos
+
+        neighbours = np.array([abs(a[line][collumn] - a[line+i][collumn+j]) for i in range(-width, width+1) for j in range(-width, width+1) if ((line+i>=0 and collumn+j>=0 and line+i<len(a) and collumn+j<len(a[0])) and (not(i==0 and j==0)))])
+        total = np.sum(neighbours)
+        n_neighbours = len(neighbours)
+
+        ADA = total/n_neighbours
         if ADA >= (media - desvio):
             true_hotpixels.append([line,collumn])
 
     true_hotpixels = np.array(true_hotpixels)
 
     return true_hotpixels
-
-
-def axu_gammaCorrection(data, gamma, max):
-    x_res, y_res = data.shape
-    for line in range(x_res):
-        for collumn in range(y_res):
-            data[line][collumn] = max * ((data[line][collumn] / max) ** (1/gamma)) #data[line][collumn] ** gamma
-    return data
-
-#10
-def gammaCorrection(hdu, gamma=0.4):
-    data = np.copy(hdu["PRIMARY"].data)
-    max = data.max()
-    k = int(len(data[0])/12)
-    inicio = 0
-    fim = k
-    data_final = []
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        process1 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
-        inicio += k
-        fim += k
-        process2 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
-        inicio += k
-        fim += k
-        process3 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
-        inicio += k
-        fim += k
-        process4 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
-        inicio += k
-        fim += k
-        process5 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
-        inicio += k
-        fim += k
-        process6 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
-        inicio += k
-        fim += k
-        process7 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
-        inicio += k
-        fim += k
-        process8 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
-        inicio += k
-        fim += k
-        process9 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
-        inicio += k
-        fim += k
-        process10 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
-        inicio += k
-        fim += k
-        process11 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
-        inicio += k
-        fim += k
-        process12 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
-        data_final = np.concatenate((process1.result(), process2.result(),process3.result(),process4.result(),process5.result(),process6.result(),process7.result(),process8.result(),process9.result(),process10.result(),process11.result(),process12.result()), axis=0)
-        return data_final
-    
-def aux_histogramSpread(data, blackPoint, whitePoint, max):
-    x_res, y_res = data.shape
-    for line in range(x_res):
-        for collumn in range(y_res):
-            if (data[line][collumn] < blackPoint):
-                z = 0
-            elif (data[line][collumn] > whitePoint):
-                z = 1
-            else:
-                z = (data[line][collumn] - blackPoint)  / (whitePoint - data[line][collumn])
-            data[line][collumn] = z * max
-    return data
-
-#11
-def histogramSpread(hdu, blackPoint=0, whitePoint=65535):
-    data = np.copy(hdu["PRIMARY"].data)
-    max = data.max()
-    k = int(len(data[0])/12)
-    inicio = 0
-    fim = k
-    data_final = []
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        process1 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, max)
-        inicio += k
-        fim += k
-        process2 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, max)
-        inicio += k
-        fim += k
-        process3 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, max)
-        inicio += k
-        fim += k
-        process4 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, max)
-        inicio += k
-        fim += k
-        process5 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, max)
-        inicio += k
-        fim += k
-        process6 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, max)
-        inicio += k
-        fim += k
-        process7 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, max)
-        inicio += k
-        fim += k
-        process8 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, max)
-        inicio += k
-        fim += k
-        process9 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, max)
-        inicio += k
-        fim += k
-        process10 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, max)
-        inicio += k
-        fim += k
-        process11 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, max)
-        inicio += k
-        fim += k
-        process12 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, max)
-        data_final = np.concatenate((process1.result(), process2.result(),process3.result(),process4.result(),process5.result(),process6.result(),process7.result(),process8.result(),process9.result(),process10.result(),process11.result(),process12.result()), axis=0)
-        return data_final
--- main.py
+++ main.py
@@ -14,66 +14,68 @@ def true_hotpixels():
             desvio = 2*np.std(darkframe_data)
             limit = media + desvio
             hot_pixels = functions.get_hotpixels(darkframe, limit)
-            return functions.get_true_hotpixels(hdu, hot_pixels, media, desvio, raio=2)
+            return functions.get_true_hotpixels(hdu, hot_pixels, media, desvio, width=2)
 
 def make_filtered_image():
     with fits.open(sys.argv[1]) as darkframe:
         limit = np.mean(darkframe["PRIMARY"].data) + 2*np.std(darkframe["PRIMARY"].data)
         hot_pixels = functions.get_hotpixels(darkframe,limit)
         with fits.open(sys.argv[2]) as hdu:
-            functions.remove_hotpixels(hdu, hot_pixels, raio=2)
+            functions.remove_hotpixels(hdu, hot_pixels, width=2)
 
 def make_stacked_image():
     with fits.open(sys.argv[1]) as darkframe:
         with fits.open(sys.argv[2]) as direct:
-            functions.get_stacked_image(darkframe, direct, raio=5)
+            functions.get_stacked_image(darkframe, direct, width=2)
 
 def make_gamma():
     with fits.open(sys.argv[1]) as hdu:
-            final_data = functions.gammaCorrection(hdu)
+            final_data = functions.corrections.gammaCorrection(hdu)
             img = fits.PrimaryHDU(final_data)
-            img.writeto(functions.new_name("gamma.fits"), overwrite=True)
+            img.writeto(functions.name_gen("gamma.fits"), overwrite=True)
 
 def make_histogramSpread():
     with fits.open(sys.argv[1]) as hdu:
-            final_data = functions.histogramSpread(hdu)
+            final_data = functions.corrections.histogramSpread(hdu)
             img = fits.PrimaryHDU(final_data)
-            img.writeto(functions.new_name("gamma.fits"), overwrite=True)
+            img.writeto(functions.name_gen("gamma.fits"), overwrite=True)
 
-def make_gamma_from_filtered_image(raio=1):
+def make_gamma_from_filtered_image(width=1):
     with fits.open(sys.argv[1]) as darkframe:
         with fits.open(sys.argv[2]) as hdu:
             limit = np.mean(darkframe["PRIMARY"].data) + 2*np.std(darkframe["PRIMARY"].data)
             hot_pixels = functions.get_hotpixels(darkframe, limit)
-            new_data = functions.get_filtered_image_data(hdu["PRIMARY"].data, hot_pixels, raio=raio)
+            new_data = functions.get_filtered_image_data(hdu["PRIMARY"].data, hot_pixels, width=width)
 
             hdul = fits.PrimaryHDU(new_data)
             hdul.writeto("test.fits", overwrite=True)
 
             with fits.open("test.fits") as temp:
-                final_data = functions.gammaCorrection(temp)
+                final_data = functions.corrections.gammaCorrection(temp)
                 img = fits.PrimaryHDU(final_data)
                 img.writeto("test.fits", overwrite=True)
 
-def make_histogramSpread_from_filtered_image(raio=1):
+def make_histogramSpread_from_filtered_image(width=1):
     with fits.open(sys.argv[1]) as darkframe:
         with fits.open(sys.argv[2]) as hdu:
             limit = np.mean(darkframe["PRIMARY"].data) + 2*np.std(darkframe["PRIMARY"].data)
             hot_pixels = functions.get_hotpixels(darkframe, limit)
-            new_data = functions.get_filtered_image_data(hdu["PRIMARY"].data, hot_pixels, raio=raio)
+            new_data = functions.get_filtered_image_data(hdu["PRIMARY"].data, hot_pixels, width=width)
 
             hdul = fits.PrimaryHDU(new_data)
             hdul.writeto("test.fits", overwrite=True)
 
             with fits.open("test.fits") as temp:
-                final_data = functions.histogramSpread(temp)
+                final_data = functions.corrections.histogramSpread(temp)
                 img = fits.PrimaryHDU(final_data)
                 img.writeto("test.fits", overwrite=True)
 
-def make_plots(multiplicadores=[0, 0.5, 1,1.5, 2, 2.5, 3], raio=1):
+def make_plots(multiplicadores=[0, 0.5, 1,1.5, 2, 2.5, 3], width=1):
     with fits.open(sys.argv[1]) as darkframe:
         with fits.open(sys.argv[2]) as hdu:
             functions.plot_ADA(darkframe, hdu)
 
 if __name__ == "__main__":
     make_plots()
+    #make_filtered_image()
+    #make_gamma()
