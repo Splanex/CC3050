c0f99a0cf9ed275d46f78547a82a11f0339d5060
Splanex <piopapapigrafo@gmail.com>
2022-12-13 10:35:27 +0000

    update 13/12

--- functions.py
+++ functions.py
@@ -7,21 +7,27 @@ import glob
 import concurrent.futures
 import corrections
 
-
 def name_gen(file_name):
     i=0
     check_name = file_name
     while exists(check_name):
         i+=1
         check_name = file_name[:-5]+"_new"+str(i)+".fits"
-    print(f"Novo ficheiro gerado: {check_name}")
     return check_name
-
 
 def get_hotpixels(hdul, limit):
     data = hdul["PRIMARY"].data
     return np.argwhere(data >= limit)
 
+def get_neighbours(data,x_res,y_res,line,collumn,width):
+    return (np.array([data[line+i][collumn+j] for i in range(-width, width+1) for j in range(-width, width+1) if ((line+i>=0 and collumn+j>=0 and line+i<x_res and collumn+j<y_res) and (not(i==0 and j==0)))]))
+
+def scatter_plot(hotpixels_values,average_abs_diff, k):
+    plt.scatter(hotpixels_values,average_abs_diff)
+    plt.xlabel('Hot values')
+    plt.ylabel('Absolute difference average')
+    plt.title("K = {}".format(k))
+    plt.show()
 
 def remove_hotpixels(hdu, hot_pixels, width=1):
     a = np.copy(hdu["PRIMARY"].data)
@@ -29,7 +35,7 @@ def remove_hotpixels(hdu, hot_pixels, width=1):
     for k in hot_pixels:
         line = k[0]
         collumn = k[1]
-        neighbours = np.array([a[line+i][collumn+j] for i in range(-width, width+1) for j in range(-width, width+1) if ((line+i>=0 and collumn+j>=0 and line+i<x_res and collumn+j<y_res) and (not(i==0 and j==0)))])
+        neighbours = get_neighbours(a,x_res,y_res,line,collumn,width)
         mediana = np.median(neighbours)
         a[line][collumn] = mediana
 
@@ -39,10 +45,10 @@ def remove_hotpixels(hdu, hot_pixels, width=1):
     try:
         hdul = fits.PrimaryHDU(a)
         hdul.writeto(new_name,overwrite=True)
+        print(f"Novo ficheiro gerado: {new_name}")
 
     except Exception as e:
         print(e)
-
 
 def get_filtered_image_data(hdu_data, hot_pixels, width):
     a = np.copy(hdu_data)
@@ -50,7 +56,7 @@ def get_filtered_image_data(hdu_data, hot_pixels, width):
     for k in hot_pixels:
         line = k[0]
         collumn = k[1]
-        neighbours = np.array([a[line+i][collumn+j] for i in range(-width, width+1) for j in range(-width, width+1) if ((line+i>=0 and collumn+j>=0 and line+i<x_res and collumn+j<y_res) and (not(i==0 and j==0)))])
+        neighbours = get_neighbours(a,x_res,y_res,line,collumn,width)
         mediana = np.median(neighbours)
         a[line][collumn] = mediana
 
@@ -76,21 +82,69 @@ def get_stacked_image(darkframe, imgs_direct, std_mult=2, width=1):
     hdu.writeto(imgs_direct+"stacked.fits", overwrite=True)
     print(f"Ficheiro stacked gerado: stacked.fits")
 
-def get_true_hotpixels(hdul, hot_pixels, media, desvio, width=1):
-    a = np.copy(hdul["PRIMARY"].data)
-    true_hotpixels = []
+
+def get_ADA(value, neighbours):
+    total = 0
+    for neighbour in neighbours:
+        if (value >= neighbour):
+            total += value - neighbour
+        else:
+            total += neighbour - value
+    return (total/len(neighbours))
+
+
+def remove_resistant_hotpixels(hdu,width=1):
+    data_copy = np.copy(hdu["PRIMARY"].data)
+    x_res,y_res = data_copy.shape
+
+    #Obter lista de potenciais pixeis quentes não presentes no darkframe
+    limit = np.mean(hdu["PRIMARY"].data) + 0.057*np.std(hdu["PRIMARY"].data)
+    hot_pixels = get_hotpixels(hdu, limit)
+
     for k in hot_pixels:
         line = k[0]
         collumn = k[1]
+        print(f"{line}:{collumn}")
+        neighbours = get_neighbours(data_copy,x_res,y_res,line,collumn,width)
+        average_abs_diff = get_ADA(data_copy[line][collumn], neighbours)
+        if (average_abs_diff >= 100):
+            mediana = np.median(neighbours)
+            data_copy[line][collumn] = mediana
 
-        neighbours = np.array([abs(a[line][collumn] - a[line+i][collumn+j]) for i in range(-width, width+1) for j in range(-width, width+1) if ((line+i>=0 and collumn+j>=0 and line+i<len(a) and collumn+j<len(a[0])) and (not(i==0 and j==0)))])
-        total = np.sum(neighbours)
-        n_neighbours = len(neighbours)
+    file_name = hdu.filename()
+    new_name = name_gen(file_name)
 
-        ADA = total/n_neighbours
-        if ADA >= (media - desvio):
-            true_hotpixels.append([line,collumn])
+    try:
+        hdul = fits.PrimaryHDU(data_copy)
+        hdul.writeto(new_name,overwrite=True)
+        print(f"Novo ficheiro gerado: {new_name}")
 
-    true_hotpixels = np.array(true_hotpixels)
+    except Exception as e:
+        print(e)
 
-    return true_hotpixels
+def remove_all_resistant_hotpixels(hdu,width=1):
+    data_copy = np.copy(hdu["PRIMARY"].data)
+    x_res,y_res = data_copy.shape
+
+    all_pixels = np.argwhere(data_copy >= 0)
+
+    for k in all_pixels:
+        line = k[0]
+        collumn = k[1]
+        print(f"{line}:{collumn}")
+        neighbours = get_neighbours(data_copy,x_res,y_res,line,collumn,width)
+        average_abs_diff = get_ADA(data_copy[line][collumn], neighbours)
+        if (average_abs_diff >= 100):
+            mediana = np.median(neighbours)
+            data_copy[line][collumn] = mediana
+
+    file_name = hdu.filename()
+    new_name = name_gen(file_name)
+
+    try:
+        hdul = fits.PrimaryHDU(data_copy)
+        hdul.writeto(new_name,overwrite=True)
+        print(f"Novo ficheiro gerado: {new_name}")
+
+    except Exception as e:
+        print(e)
--- main.py
+++ main.py
@@ -6,19 +6,22 @@ import sys
 import glob
 import functions
 
-def true_hotpixels():
+def filter_darkframe():
     with fits.open(sys.argv[1]) as darkframe:
+        temp[temp < 494] = 0
+        img = fits.PrimaryHDU(temp)
+        img.writeto("test18.fits", overwrite=True)
+
+def test_make_filtered_image():
+    with fits.open(sys.argv[1]) as darkframe:
+        hot_pixels = np.argwhere(darkframe["PRIMARY"].data != 0)
         with fits.open(sys.argv[2]) as hdu:
-            darkframe_data = np.copy(darkframe["PRIMARY"].data)
-            media = np.mean(darkframe_data)
-            desvio = 2*np.std(darkframe_data)
-            limit = media + desvio
-            hot_pixels = functions.get_hotpixels(darkframe, limit)
-            return functions.get_true_hotpixels(hdu, hot_pixels, media, desvio, width=2)
+            functions.remove_hotpixels(hdu, hot_pixels, width=2)
 
 def make_filtered_image():
     with fits.open(sys.argv[1]) as darkframe:
         limit = np.mean(darkframe["PRIMARY"].data) + 2*np.std(darkframe["PRIMARY"].data)
+        #limit = np.mean(darkframe["PRIMARY"].data) + 0.057*np.std(darkframe["PRIMARY"].data)
         hot_pixels = functions.get_hotpixels(darkframe,limit)
         with fits.open(sys.argv[2]) as hdu:
             functions.remove_hotpixels(hdu, hot_pixels, width=2)
@@ -45,12 +48,13 @@ def make_histogramSpread():
 def make_gamma_from_filtered_image(width=1):
     with fits.open(sys.argv[1]) as darkframe:
         with fits.open(sys.argv[2]) as hdu:
-            limit = np.mean(darkframe["PRIMARY"].data) + 2*np.std(darkframe["PRIMARY"].data)
+            #limit = np.mean(darkframe["PRIMARY"].data) + 2*np.std(darkframe["PRIMARY"].data)
+            limit = np.mean(darkframe["PRIMARY"].data) + 0.057*np.std(darkframe["PRIMARY"].data)
             hot_pixels = functions.get_hotpixels(darkframe, limit)
             new_data = functions.get_filtered_image_data(hdu["PRIMARY"].data, hot_pixels, width=width)
 
             hdul = fits.PrimaryHDU(new_data)
-            new_name = functions.name_gen("Gamma.fits")
+            new_name = functions.name_gen("Gamma_from_filtered_image.fits")
             hdul.writeto(new_name, overwrite=True)
 
             with fits.open(new_name) as temp:
@@ -61,12 +65,13 @@ def make_gamma_from_filtered_image(width=1):
 def make_histogramSpread_from_filtered_image(width=1):
     with fits.open(sys.argv[1]) as darkframe:
         with fits.open(sys.argv[2]) as hdu:
-            limit = np.mean(darkframe["PRIMARY"].data) + 2*np.std(darkframe["PRIMARY"].data)
+            #limit = np.mean(darkframe["PRIMARY"].data) + 2*np.std(darkframe["PRIMARY"].data)
+            limit = np.mean(darkframe["PRIMARY"].data) + 0.057*np.std(darkframe["PRIMARY"].data)
             hot_pixels = functions.get_hotpixels(darkframe, limit)
             new_data = functions.get_filtered_image_data(hdu["PRIMARY"].data, hot_pixels, width=width)
 
             hdul = fits.PrimaryHDU(new_data)
-            new_name = functions.name_gen("HistogramSpread.fits")
+            new_name = functions.name_gen("HistogramSpread_from_filtered_image.fits")
             hdul.writeto(new_name, overwrite=True)
 
             with fits.open(new_name) as temp:
@@ -74,11 +79,22 @@ def make_histogramSpread_from_filtered_image(width=1):
                 img = fits.PrimaryHDU(final_data)
                 img.writeto(new_name, overwrite=True)
 
+def remove_resistant():
+    with fits.open(sys.argv[1]) as hdu:
+        functions.remove_resistant_hotpixels(hdu)
+
+def remove_all_resistant():
+    with fits.open(sys.argv[1]) as hdu:
+        functions.remove_all_resistant_hotpixels(hdu)
+
 if __name__ == "__main__":
-    #make_filtered_image()
-    #make_stacked_image()
-    #print(true_hotpixels())
-    make_gamma()
-    #make_histogramSpread()
-    #make_gamma_from_filtered_image()
-    #make_histogramSpread_from_filtered_image()
+    #make_filtered_image() X
+    #test_make_filtered_image() X
+    #make_stacked_image() X
+    #make_gamma() X
+    #make_histogramSpread() X
+    #make_gamma_from_filtered_image()  X
+    #make_histogramSpread_from_filtered_image() X
+    #filter_darkframe() X
+    #remove_resistant() X
+    #remove_all_resistant() X
