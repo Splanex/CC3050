faeb198aee20fbe83387462c97467c7889ddffbd
Splanex <piopapapigrafo@gmail.com>
2022-12-15 12:13:27 +0000

    Update 15/12

---
+++ backup/v2/corrections.py
@@ -0,0 +1,139 @@
+from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
+from os.path import exists
+import sys
+import glob
+import concurrent.futures
+
+def axu_gammaCorrection(data, gamma, max):
+    x_res, y_res = data.shape
+    for line in range(x_res):
+        for collumn in range(y_res):
+            data[line][collumn] = max * ((data[line][collumn] / max) ** (1/gamma)) 
+#data[line][collumn] ** gamma
+    #data = [(max * ((data[line][collumn] / max) ** (1/gamma))) for line in range(x_res) for 
+collumn in range(y_res)]        
+    return data
+
+
+def gammaCorrection(hdu, gamma=0.4):
+    data = np.copy(hdu["PRIMARY"].data)
+    max = data.max()
+    k = int(len(data[0])/12)
+    inicio = 0
+    fim = k
+    data_final = []
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        process1 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
+        inicio += k
+        fim += k
+        process2 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
+        inicio += k
+        fim += k
+        process3 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
+        inicio += k
+        fim += k
+        process4 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
+        inicio += k
+        fim += k
+        process5 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
+        inicio += k
+        fim += k
+        process6 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
+        inicio += k
+        fim += k
+        process7 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
+        inicio += k
+        fim += k
+        process8 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
+        inicio += k
+        fim += k
+        process9 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
+        inicio += k
+        fim += k
+        process10 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
+        inicio += k
+        fim += k
+        process11 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
+        inicio += k
+        fim += k
+        process12 = executor.submit(axu_gammaCorrection, data[inicio:fim], gamma, max)
+        data_final = np.concatenate((process1.result(), 
+process2.result(),process3.result(),process4.result(),process5.result(),process6.result(),process7.result(),process8.result(),process9.result(),process10.result(),process11.result(),process12.result()), 
+axis=0)
+        return data_final
+
+
+def aux_histogramSpread(data, blackPoint, whitePoint, max):
+    x_res, y_res = data.shape
+    for line in range(x_res):
+        for collumn in range(y_res):
+            if (data[line][collumn] < blackPoint):
+                z = 0
+            elif (data[line][collumn] > whitePoint):
+                z = 1
+            else:
+                z = (data[line][collumn] - blackPoint)  / (whitePoint - data[line][collumn])
+            data[line][collumn] = z * max
+    return data
+
+
+def histogramSpread(hdu, blackPoint=0, whitePoint=65535):
+    data = np.copy(hdu["PRIMARY"].data)
+    max = data.max()
+    k = int(len(data[0])/12)
+    inicio = 0
+    fim = k
+    data_final = []
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        process1 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, 
+max)
+        inicio += k
+        fim += k
+        process2 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, 
+max)
+        inicio += k
+        fim += k
+        process3 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, 
+max)
+        inicio += k
+        fim += k
+        process4 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, 
+max)
+        inicio += k
+        fim += k
+        process5 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, 
+max)
+        inicio += k
+        fim += k
+        process6 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, 
+max)
+        inicio += k
+        fim += k
+        process7 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, 
+max)
+        inicio += k
+        fim += k
+        process8 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, 
+max)
+        inicio += k
+        fim += k
+        process9 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, 
+max)
+        inicio += k
+        fim += k
+        process10 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, 
+max)
+        inicio += k
+        fim += k
+        process11 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, 
+max)
+        inicio += k
+        fim += k
+        process12 = executor.submit(aux_histogramSpread, data[inicio:fim], blackPoint, whitePoint, 
+max)
+        data_final = np.concatenate((process1.result(), 
+process2.result(),process3.result(),process4.result(),process5.result(),process6.result(),process7.result(),process8.result(),process9.result(),process10.result(),process11.result(),process12.result()), 
+axis=0)
+        return data_final
---
+++ backup/v2/main.py
@@ -0,0 +1,131 @@
+from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
+from os.path import exists
+import sys
+import glob
+import functions
+
+def filter_darkframe():
+    with fits.open(sys.argv[1]) as darkframe:
+        temp[temp < 494] = 0
+        img = fits.PrimaryHDU(temp)
+        img.writeto("test18.fits", overwrite=True)
+
+def test_make_filtered_image():
+    with fits.open(sys.argv[1]) as darkframe:
+        hot_pixels = np.argwhere(darkframe["PRIMARY"].data != 0)
+        with fits.open(sys.argv[2]) as hdu:
+            functions.remove_hotpixels(hdu, hot_pixels, width=2)
+
+def make_filtered_image():
+    with fits.open(sys.argv[1]) as darkframe:
+        #limit = np.mean(darkframe["PRIMARY"].data) + 2*np.std(darkframe["PRIMARY"].data)
+        limit = np.mean(darkframe["PRIMARY"].data) + 0.057*np.std(darkframe["PRIMARY"].data)
+        hot_pixels = functions.get_hotpixels(darkframe,limit)
+        with fits.open(sys.argv[2]) as hdu:
+            functions.remove_hotpixels(hdu, hot_pixels, width=2)
+
+def make_stacked_image():
+    with fits.open(sys.argv[1]) as darkframe:
+        directory = sys.argv[2]
+        if directory[-1] != '/':
+            directory += '/'
+            functions.get_stacked_image(darkframe, directory, width=2)
+
+def make_gamma():
+    with fits.open(sys.argv[1]) as hdu:
+            final_data = functions.corrections.gammaCorrection(hdu)
+            img = fits.PrimaryHDU(final_data)
+            img.writeto(functions.name_gen("Gamma.fits"), overwrite=True)
+
+def make_histogramSpread():
+    with fits.open(sys.argv[1]) as hdu:
+            final_data = functions.corrections.histogramSpread(hdu)
+            img = fits.PrimaryHDU(final_data)
+            img.writeto(functions.name_gen("HistogramSpread.fits"), overwrite=True)
+
+def make_gamma_from_filtered_image(width=1):
+    with fits.open(sys.argv[1]) as darkframe:
+        with fits.open(sys.argv[2]) as hdu:
+            #limit = np.mean(darkframe["PRIMARY"].data) + 2*np.std(darkframe["PRIMARY"].data)
+            limit = np.mean(darkframe["PRIMARY"].data) + 0.057*np.std(darkframe["PRIMARY"].data)
+            hot_pixels = functions.get_hotpixels(darkframe, limit)
+            new_data = functions.get_filtered_image_data(hdu["PRIMARY"].data, hot_pixels, 
+width=width)
+
+            hdul = fits.PrimaryHDU(new_data)
+            new_name = functions.name_gen("Gamma_from_filtered_image.fits")
+            hdul.writeto(new_name, overwrite=True)
+
+            with fits.open(new_name) as temp:
+                final_data = functions.corrections.gammaCorrection(temp)
+                img = fits.PrimaryHDU(final_data)
+                img.writeto(new_name, overwrite=True)
+
+def make_histogramSpread_from_filtered_image(width=1):
+    with fits.open(sys.argv[1]) as darkframe:
+        with fits.open(sys.argv[2]) as hdu:
+            #limit = np.mean(darkframe["PRIMARY"].data) + 2*np.std(darkframe["PRIMARY"].data)
+            limit = np.mean(darkframe["PRIMARY"].data) + 0.057*np.std(darkframe["PRIMARY"].data)
+            hot_pixels = functions.get_hotpixels(darkframe, limit)
+            new_data = functions.get_filtered_image_data(hdu["PRIMARY"].data, hot_pixels, 
+width=width)
+
+            hdul = fits.PrimaryHDU(new_data)
+            new_name = functions.name_gen("HistogramSpread_from_filtered_image.fits")
+            hdul.writeto(new_name, overwrite=True)
+
+            with fits.open(new_name) as temp:
+                final_data = functions.corrections.histogramSpread(temp)
+                img = fits.PrimaryHDU(final_data)
+                img.writeto(new_name, overwrite=True)
+
+def remove_resistant():
+    with fits.open(sys.argv[1]) as hdu:
+        functions.remove_resistant_hotpixels(hdu)
+
+def remove_all_resistant():
+    with fits.open(sys.argv[1]) as hdu:
+        functions.remove_all_resistant_hotpixels(hdu)
+
+def PSNR():
+    with fits.open(sys.argv[1]) as hdu1:
+        with fits.open(sys.argv[2]) as hdu2:
+            hdu1_copy = np.copy(hdu1["PRIMARY"].data)
+            hdu2_copy = np.copy(hdu2["PRIMARY"].data)
+            print(functions.get_PSNR(hdu1_copy,hdu2_copy))
+
+def make_noisy_file():
+    with fits.open(sys.argv[1]) as hdu:
+        data_copy = np.copy(hdu["PRIMARY"].data)
+        background_value = functions.get_background_value(data_copy)
+        x_res, y_res = data_copy.shape
+
+        x_index, y_index, noise = functions.gen_noise(background_value, x_res, y_res)
+
+        noisy_image_data = functions.gen_noisy_image_data(x_index, y_index, noise, data_copy)
+
+        new_name = "Noisy_image.fits"
+
+        try:
+            hdul = fits.PrimaryHDU(noisy_image_data)
+            hdul.writeto(new_name,overwrite=True)
+            print(f"Novo ficheiro gerado: {new_name}")
+
+        except Exception as e:
+            print(e)
+
+if __name__ == "__main__":
+    #make_filtered_image() X
+    #test_make_filtered_image() X
+    #make_stacked_image() X
+    #make_gamma() X
+    #make_histogramSpread() X
+    #make_gamma_from_filtered_image()  X
+    #make_histogramSpread_from_filtered_image() X
+    #filter_darkframe() X
+    #remove_resistant() X
+    #remove_all_resistant()X
+    #PSNR()
+    #make_noisy_file()
